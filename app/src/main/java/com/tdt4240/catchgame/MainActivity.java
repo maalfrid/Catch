8a30836fd70d2de0d7ead26cffe361c569e8e95e
Molly <malfridha@gmail.com>
2019-03-11 10:38:18 +0100

    Initinal commit

---
+++ app/src/main/java/com/tdt4240/catchgame/MainActivity.java
@@ -0,0 +1,24 @@
+package com.tdt4240.catchgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.android.gms.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*
+    * TODO @Anna, @Molly
+    * Main homepage including:
+    * - Play game
+    * - Settings
+    * - Highscore
+    * - Tutorial/Rules
+    * - Evt. about
+    * */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
