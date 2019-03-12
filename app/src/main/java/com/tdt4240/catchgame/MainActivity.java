e0b26b330895765c3d8d46d22298f67f339caa0b
Molly <malfridha@gmail.com>
2019-03-12 18:47:01 +0100

    Basic layout for main menu and settings menu (obs: the constraint layout is not fixed yet)

--- app/src/main/java/com/tdt4240/catchgame/MainActivity.java
+++ app/src/main/java/com/tdt4240/catchgame/MainActivity.java
@@ -1,24 +1,32 @@
 package com.tdt4240.catchgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.google.android.gms.*;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    /*
-    * TODO @Anna, @Molly
-    * Main homepage including:
-    * - Play game
-    * - Settings
-    * - Highscore
-    * - Tutorial/Rules
-    * - Evt. about
-    * */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Buttons and navigation
+        Button playBtn = findViewById(R.id.btn_play);
+        Button settingsBtn = findViewById(R.id.btn_settings);
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {startActivity(new Intent(v.getContext(), PlayMenuActivity.class));
+            }
+        });
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {startActivity(new Intent(v.getContext(), SettingsActivity.class));
+            }
+        });
     }
 }
