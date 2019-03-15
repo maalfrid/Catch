4b2c45f77fde09a29b66974821814796a25473ad
Cathrine Akre-Aas <cathrine-97@hotmail.com>
2019-03-15 08:58:29 +0100

    Basic layout for buttons in playmenu

--- app/src/main/java/com/tdt4240/catchgame/PlayMenuActivity.java
+++ app/src/main/java/com/tdt4240/catchgame/PlayMenuActivity.java
@@ -1,7 +1,10 @@
 package com.tdt4240.catchgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PlayMenuActivity extends AppCompatActivity {
 
@@ -25,5 +28,23 @@ public class PlayMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_menu);
+
+        //Buttons and navigation
+        Button singleBtn = findViewById(R.id.btn_single);
+        Button mulitBtn = findViewById(R.id.btn_multi);
+
+        singleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), SinglePlayerActivity.class));
+            }
+        });
+
+        mulitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), MultiPlayerActivity.class));
+            }
+        });
     }
 }
