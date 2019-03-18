3b1383c82d68691cb497120375741b01ea4a2146
Molly <maalfria@stud.ntnu.no>
2019-03-18 09:44:24 +0100

    Removed, logic implemented in other branch

--- app/src/main/java/com/tdt4240/catchgame/MainActivity.java
+++ app/src/main/java/com/tdt4240/catchgame/MainActivity.java
@@ -9,28 +9,10 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    /*
-    * TODO @Anna, @Molly
-    * Main homepage including:
-    * - Play game
-    * - Settings
-    * - Highscore
-    * - Tutorial/Rules
-    * - Evt. about
-    * */
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Buttons and navigation
-        Button playBtn = findViewById(R.id.btn_play);
-
-        playBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {startActivity(new Intent(v.getContext(), PlayMenuActivity.class));
-            }
-        });
     }
 }
