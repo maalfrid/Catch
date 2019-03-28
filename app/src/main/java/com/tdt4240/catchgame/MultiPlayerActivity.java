e0cd745cc68b3c91bf423c052edc983027391f3f
Abhinav Padala <abhinavpadala@live.com>
2019-03-28 14:48:07 +0100

    Default google sign in button

--- app/src/main/java/com/tdt4240/catchgame/MultiPlayerActivity.java
+++ app/src/main/java/com/tdt4240/catchgame/MultiPlayerActivity.java
@@ -3,12 +3,25 @@ package com.tdt4240.catchgame;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-import com.google.android.gms.auth.signin.GoogleSignIn;
+//Google Sign In feature
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 
 
-public class MultiPlayerActivity extends Activity {
+
+
+public class MultiPlayerActivity extends Activity implements
+        View.OnClickListener {
+
+
+    final static String TAG = "Catch";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +29,15 @@ public class MultiPlayerActivity extends Activity {
         setContentView(R.layout.activity_multi_player);
 
         System.out.println("Test");
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.button_sign_in:
+                // start the sign-in flow
+                Log.d(TAG, "------------------Sign-in button clicked");
+                //startSignInIntent();
+        }
     }
 }
