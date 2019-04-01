55f9763ce1bd42c5718b4cd485c88c9050607343
Anna <annaroarsen@gmail.com>
2019-04-01 14:34:14 +0200

    added backgroundmusic in single player

--- app/src/main/java/com/tdt4240/catchgame/SinglePlayerActivity.java
+++ app/src/main/java/com/tdt4240/catchgame/SinglePlayerActivity.java
@@ -1,11 +1,15 @@
 package com.tdt4240.catchgame;
 
+import android.media.MediaPlayer;
+import android.media.PlaybackParams;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SinglePlayerActivity extends AppCompatActivity {
     private String difficulty;
 
+    MediaPlayer backgroundMusic;
 
     public SinglePlayerActivity() {
     }
@@ -16,13 +20,27 @@ public class SinglePlayerActivity extends AppCompatActivity {
         setContentView(new GameView(this, this));
         this.difficulty = getIntent().getStringExtra("difficulty");
 
+        backgroundMusic = MediaPlayer.create(this, R.raw.test_song);
+
+
+        backgroundMusic.setLooping(true);
+        backgroundMusic.setVolume(10.0f, 3.0f);
+        
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        backgroundMusic.start();
+    }
+
 
     @Override
     protected void onPause(){
         MainThread.setRunning(false);
         super.onPause();
+        backgroundMusic.release();
     }
 
     public String getDifficulty(){
