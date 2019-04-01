3b6eeabd30f4689a47792ae40943af954a3d5ec3
Abhinav Padala <abhinavpadala@live.com>
2019-04-01 11:38:15 +0200

    Sign In and Sign out features

--- app/src/main/java/com/tdt4240/catchgame/MultiPlayerActivity.java
+++ app/src/main/java/com/tdt4240/catchgame/MultiPlayerActivity.java
@@ -1,5 +1,6 @@
 package com.tdt4240.catchgame;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +14,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 
@@ -34,6 +36,7 @@ public class MultiPlayerActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multi_player);
+        findViewById(R.id.view_signIn).setVisibility(View.VISIBLE);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
@@ -50,7 +53,7 @@ public class MultiPlayerActivity extends AppCompatActivity implements
     }
 
     final static int[] CLICKABLEs = {
-            R.id.button_sign_in
+            R.id.button_sign_in, R.id.button_sign_out
     };
 
 
@@ -62,7 +65,12 @@ public class MultiPlayerActivity extends AppCompatActivity implements
                 Log.d(TAG, "-----------Sign-in button clicked");
                 startSignInIntent();
                 break;
+            case R.id.button_sign_out:
+                signOut();
+                break;
+
         }
+
     }
 
     /**
@@ -71,6 +79,22 @@ public class MultiPlayerActivity extends AppCompatActivity implements
      */
     public void startSignInIntent(){
         startActivityForResult(mGoogleSignInClient.getSignInIntent(), RC_SIGN_IN);
+    }
+
+    private void signOut(){
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this,
+                        new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    Log.d(TAG, " ----------Signed out successfully");
+                                } else {
+                                    Log.d(TAG, " ----------Signed out failed");
+                                }
+                            }
+                        });
+
     }
 
 
@@ -89,7 +113,7 @@ public class MultiPlayerActivity extends AppCompatActivity implements
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
 
             //Signed in successfully, show authenticated UI.
-            Log.d(TAG, "---Account:"+ account);
+            Log.d(TAG, "--------Account:"+ account);
         } catch(ApiException e){
             Log.w(TAG, "-----------signInResult:failed code=" + e.getStatusCode());
         }
