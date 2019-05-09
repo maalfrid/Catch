c4f518091779754bb45e04bd7e108b5cd4e988c5
Sigrid Sognli Høyem <48217313+SigridHoyem@users.noreply.github.com>
2019-05-09 11:09:11 +0200

    Refactor Activities

--- app/src/main/java/com/tdt4240/catchgame/Controllers/Loading.java
+++ app/src/main/java/com/tdt4240/catchgame/Controllers/Loading.java
@@ -1,6 +1,5 @@
 package com.tdt4240.catchgame.Controllers;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
--- app/src/main/java/com/tdt4240/catchgame/Controllers/SinglePlayerActivity.java
+++ app/src/main/java/com/tdt4240/catchgame/Controllers/SinglePlayerActivity.java
@@ -1,12 +1,10 @@
 package com.tdt4240.catchgame.Controllers;
 
-import android.content.Context;
 import android.media.MediaPlayer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.tdt4240.catchgame.Model.Backgrounds;
 import com.tdt4240.catchgame.R;
 import com.tdt4240.catchgame.View.GameView;
 
@@ -28,12 +26,9 @@ public class SinglePlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.background = getIntent().getStringExtra("background");
-
-
         setContentView(new GameView(this, this));
         this.difficulty = getIntent().getStringExtra("difficulty");
         this.avatar = getIntent().getStringExtra("avatar");
-       // this.background = getIntent().getStringExtra("background");
         this.backgroundsoundOn = getIntent().getExtras().getBoolean("backgroundSound");
         this.soundEffectsOn = getIntent().getExtras().getBoolean("soundEffects");
         this.backgroundMusic = MediaPlayer.create(this, R.raw.test_song);
@@ -70,10 +65,6 @@ public class SinglePlayerActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        if(this.backgroundsoundOn){
-            //this.backgroundMusic.release();
-        }
-
     }
 
     @Override
@@ -89,9 +80,7 @@ public class SinglePlayerActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         startActivity(new Intent(this, MenuActivity.class));
-        //if(this.backgroundsoundOn){
          this.backgroundMusic.release();
-        //}
 
     }
 
